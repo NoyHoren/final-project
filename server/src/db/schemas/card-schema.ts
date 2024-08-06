da36791be7ce0ec60f574f360007c6a7d56a80b4
NoyHoren <noyhoren@gmail.com>
2024-08-06 20:32:11 +0300

    backend finished

---
+++ server/src/db/schemas/card-schema.ts
@@ -0,0 +1,66 @@
+import { Schema } from "mongoose";
+import { ICard } from "../../@types/@types";
+import addressSchema from "./address-schema";
+import imageSchema from "./image-schema";
+
+
+const cardSchema = new Schema<ICard>({
+    title: {
+        type: String,
+        required: true,
+        minlength: 2,
+        maxlength: 256
+    },
+    subtitle: {
+        type: String,
+        required: true,
+        minlength: 2,
+        maxlength: 256
+    },
+    description: {
+        type: String,
+        required: true,
+        minlength: 2,
+        maxlength: 1024
+    },
+    phone: {
+        type: String,
+        required: false,
+        minlength: 9,
+        maxlength: 11
+    },
+
+    email: {
+        type: String,
+        required: false,
+        minlength: 6,
+        maxlength: 20,
+        unique: true
+    },
+    web: {
+        type: String,
+        required: false,
+        minlength: 14,
+        maxlength: 100
+    },
+    address: {
+        type: addressSchema, required: false
+    },
+    image: {
+        type: imageSchema, required: false
+    },
+
+    createdAt: {
+        type: Date,
+        required: false,
+        default: new Date()
+    },
+    userId: {
+        type: String,
+        required: true
+    },
+});
+
+
+
+export default cardSchema;
---
+++ server/src/services/card-service.ts
@@ -0,0 +1,50 @@
+import _ from "underscore";
+import { ICardInput } from "../@types/@types";
+import Card from "../db/models/card-model";
+
+export const cardService = {
+
+
+    createCard: async (data: ICardInput, userId: string) => {
+
+        const card = new Card(data);
+        card.userId = userId;
+
+        return card.save();
+    },
+
+
+    getAllCards: async () => Card.find(),
+
+
+    getUserCards: async (userId: string) => {
+        return Card.find({ userId: userId });
+    },
+
+
+    getCardById: async (id: string) => {
+        return Card.findById(id);
+    },
+
+
+    updateCard: async (id: string, data: ICardInput) => {
+        return Card.findByIdAndUpdate(id, data, { new: true });
+    },
+
+
+    likeCard: async (id: string, userId: string) => {
+        const card = await Card.findById(id);
+        if (!card) {
+            throw new Error("Card not found");
+        }
+
+        if (!card.likes.includes(userId)) {
+            card.likes.push(userId);
+        }
+        return card.save();
+    },
+
+    deleteCard: async (id: string) => {
+        return Card.findByIdAndDelete(id);
+    }
+};
